eb13ec26a2e5781f822ee040b768caea8b383e87
m7Aei <mateuspm2015@hotmail.com>
2019-07-19 00:38:22 -0300

    Criando o controller do pokemon

---
+++ src/app/controllers/PokemonController.js
@@ -0,0 +1,15 @@
+const Pokemon = require('../models/Pokemon')
+
+class PokemonController {
+  async index (req, res) {}
+
+  async store (req, res) {}
+
+  async show (req, res) {}
+
+  async update (req, res) {}
+
+  async destroy (req, res) {}
+}
+
+module.exports = new PokemonController()
