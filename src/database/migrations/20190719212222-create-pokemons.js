747c3a7c2a73bd0b8ab1fbddbfbeca9cfcf2e434
m7Aei <mateuspm2015@hotmail.com>
2019-07-19 19:48:56 -0300

    Configurando a migration dos pokemao

--- src/database/migrations/20190719212222-create-pokemons.js
+++ src/database/migrations/20190719212222-create-pokemons.js
@@ -1,23 +1,142 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('pokemons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      pokedexNumber: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      imgName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      generation: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      evolutionStage: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      evolved: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      familyID: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      crossGen: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      type1: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      type2: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      weather1: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      weather2: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      statTotal: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      ATK: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      DEF: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      STA: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      legendary: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      aquireable: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      spawns: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      regional: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      raidable: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      hatchable: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      shiny: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      nest: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      New: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      notGettable: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      futureEvolve: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      CP1: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      CP2: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created_At: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_At: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('pokemons')
   }
-};
+}
