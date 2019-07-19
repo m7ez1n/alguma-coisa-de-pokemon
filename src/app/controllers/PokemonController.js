b989131fdaee5c7740adeba49f64d8687a1fbf95
m7Aei <mateuspm2015@hotmail.com>
2019-07-19 01:48:20 -0300

    Fazendo o metodo show e o store

--- src/app/controllers/PokemonController.js
+++ src/app/controllers/PokemonController.js
@@ -3,9 +3,17 @@ const Pokemon = require('../models/Pokemon')
 class PokemonController {
   async index (req, res) {}
 
-  async show (req, res) {}
+  async show (req, res) {
+    const pokemon = await Pokemon.findById(req.params.id)
 
-  async store (req, res) {}
+    return res.json(pokemon)
+  }
+
+  async store (req, res) {
+    const pokemon = await Pokemon.create(req.body)
+
+    return res.json(pokemon)
+  }
 
   async update (req, res) {}
 
