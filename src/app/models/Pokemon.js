51fc4dc28dd5c4a875e89abc3665640dae21a167
m7Aei <mateuspm2015@hotmail.com>
2019-07-19 19:24:18 -0300

    Fazendo o model do pokemon com o sequelize

---
+++ src/app/models/Pokemon.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Pokemon = sequelize.define('Pokemon', {
+    name: DataTypes.STRING,
+    pokedexNumber: DataTypes.INTEGER,
+    imgName: DataTypes.STRING,
+    generation: DataTypes.INTEGER,
+    evolutionStage: DataTypes.STRING,
+    evolved: DataTypes.INTEGER,
+    familyID: DataTypes.INTEGER,
+    crossGen: DataTypes.INTEGER,
+    type1: DataTypes.STRING,
+    type2: DataTypes.STRING,
+    weather1: DataTypes.STRING,
+    weather2: DataTypes.STRING,
+    statTotal: DataTypes.INTEGER,
+    ATK: DataTypes.INTEGER,
+    DEF: DataTypes.INTEGER,
+    STA: DataTypes.INTEGER,
+    legendary: DataTypes.INTEGER,
+    aquireable: DataTypes.INTEGER,
+    spawns: DataTypes.INTEGER,
+    regional: DataTypes.INTEGER,
+    raidable: DataTypes.INTEGER,
+    hatchable: DataTypes.INTEGER,
+    shiny: DataTypes.INTEGER,
+    nest: DataTypes.INTEGER,
+    New: DataTypes.INTEGER,
+    notGettable: DataTypes.INTEGER,
+    futureEvolve: DataTypes.INTEGER,
+    CP1: DataTypes.INTEGER,
+    CP2: DataTypes.INTEGER
+  })
+
+  return Pokemon
+}
